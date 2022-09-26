48fa74100daa01eda652b66023c8ab117e9067a9
FatihG34 <fthgnydn34@gmail.com>
2022-09-26 16:22:26 +0300

    quiz_serializer and urls added

--- QuizApp/urls.py
+++ QuizApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from QuizApp.views import CategoryView, home, QuizView
+from QuizApp.views import CategoryListView, home, QuizListView, CategoryDetailView, QuizDetailView
 
 
 
 urlpatterns = [
     path("", home, name="home"),
-    path("category/", CategoryView.as_view() , name="category"),
-    path("quiz/", QuizView.as_view() , name="quiz"),
+    path("category/", CategoryListView.as_view(), name="category"),
+    path("category/<int:pk>", CategoryDetailView.as_view(), name="category-detail"),
+    path("quiz/", QuizListView.as_view(), name="quiz"),
+    path("quiz/<int:pk>", QuizDetailView.as_view(), name="quiz-detail"),
 ]
--- QuizApp/views.py
+++ QuizApp/views.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+from urllib import request
 from django.shortcuts import render, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,24 +9,51 @@ from QuizApp.models import Category, Quiz
 from QuizApp.serializers import CategorySerializer, QuizSerializer
 # Create your views here.
 
+
 def home(request):
     return HttpResponse("<h1>Home Page</h1>")
 
-class CategoryView(APIView):
+
+class CategoryListView(APIView):
     def get(self, request):
         category = Category.objects.all()
         serializer = CategorySerializer(category, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-class QuizView(APIView):
+
+class CategoryDetailView(APIView):
+
+    def get_object(self, pk):
+        category_instance = get_object_or_404(Category, pk=pk)
+        return category_instance
+
+    def get(self, request, pk):
+        category = self.get_object(pk=pk)
+        serialzer = CategorySerializer(category)
+        return Response(serialzer.data)
+
+    def put(self,request, pk):
+        category = self.get_object(pk=pk)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk=pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class QuizListView(APIView):
     def get(self, request):
         quiz = Quiz.objects.all()
         serializer = QuizSerializer(quiz, many=True)
@@ -36,3 +65,26 @@ class QuizView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuizDetailView(APIView):
+    def get_object(self, pk):
+        quiz_instance = get_object_or_404(Quiz, pk=pk)
+        return quiz_instance
+    
+    def get(self, request, pk):
+        quiz = self.get_object(pk=pk)
+        serializer = QuizSerializer(quiz)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        quiz = self.get_object(pk=pk)
+        serializer = QuizSerializer(quiz, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        quiz = self.get_object(pk=pk)
+        quiz.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
